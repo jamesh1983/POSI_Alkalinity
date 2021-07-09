5d44e3e65c8d3d57f0679b334059ea881de8d377
jamesh1983 <jamesh2@163.com>
2021-07-09 14:08:04 +0800

    1

--- POSI_Alkalinity/Form1.Designer.cs
+++ POSI_Alkalinity/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(285, 182);
+            this.label1.Margin = new System.Windows.Forms.Padding(1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "计算结果：";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "退出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "浓缩倍数计算结果";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- POSI_Alkalinity/Form1.cs
+++ POSI_Alkalinity/Form1.cs
@@ -12,9 +12,59 @@ namespace POSI_Alkalinity
 {
     public partial class Form1 : Form
     {
+        public double Ca = 78.0;//补充水钙硬度（按碳酸钙计）
+        public double Mg = 19.0;//补充水镁硬度（按碳酸钙计）
+        public double Na = 100.0;//补充水钠离子
+        public double Cl = 100.0;//补充水氯离子
+        public double Clmax = 1000.0;//循环水氯离子限值
+        public double Cond = 1000.0;//补充水电导率
+        public double Alkalinity_Max = 680.0;//循环水碱度控制限值
+        public double Alkalinity_Input = 198.0;//补充水碱度
+        public double Alkalinity = 0.0;//POSI计算出的补充水碱度（当量）
+        public double SO4 = 50.0;//补充水硫酸根
+        public double SiO2 = 50.0;//补充水硅酸
+
+        public const double C_Ca = 2.45;//碳酸钙与钙离子转换常数
+        public const double C_Mg = 4.11;//碳酸钙与镁离子转换常数
+        public const double Ca_Alkalinity_Max = 1100.0;//水钙硬+全碱度限制
+        public const double Cl_SO4_Max = 2500.0;//氯离子+硫酸根限制
+        public const double Mg_SiO2_Max = 50000.0;//镁离子乘以硅酸限制
+
+        public const double Delta_Coc = 0.01;//
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();//窗口初始化
+            double COC_Cl = Math.Round((Clmax / Cl), 2);//计算按氯离子限制最大浓缩倍数
+
+            double Ca_Alkalinity = (Ca + Alkalinity) * COC_Cl;//计算循环水钙硬+全碱度
+            double Cl_SO4 = (Cl + SO4) * COC_Cl;//计算循环水氯离子+硫酸根
+            double Mg_SiO2 = (Mg * SiO2) * COC_Cl;//计算镁离子+硅酸
+
+            do//按POSI计算循环水碱度限值
+            {
+                Alkalinity = Math.Round((COC_Cl * Math.Pow(10, (1 / (Math.Log10(COC_Cl) * Math.Log10(Ca * Mg / (Cl + Na))) + 1))), 2);
+                COC_Cl = COC_Cl - Delta_Coc;
+            } while (Alkalinity_Max < Alkalinity);//比较碱度限值和碱度控制上限
+            COC_Cl += Delta_Coc;//调回一格步长
+            COC_Cl = Math.Round(COC_Cl, 2);//将逼近计算后COC保留两位小数
+
+            //do
+            //{
+            //    Ca_Alkalinity = (Ca + Alkalinity) * COC_Cl;
+            //    Cl_SO4 = (Cl + SO4) * COC_Cl;
+            //    Mg_SiO2 = (Mg * SiO2) * COC_Cl;
+            //    COC_Cl = COC_Cl - Delta_Coc;
+            //} while ((Ca_Alkalinity > Ca_Alkalinity_Max) || (Cl_SO4 > Cl_SO4_Max) || (Mg_SiO2 > Mg_SiO2_Max));
+            //COC_Cl += Delta_Coc;
+
+            string result = "最大浓缩倍数：" + COC_Cl.ToString();//生成需要展示的计算结果字符串
+            label1.Text = result;//将字符串赋值入控件
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();//关闭窗口
         }
     }
 }
